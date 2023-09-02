81a2b787d358471856876960ba88d625e8b35fa4
lxspector <lxspector@gmail.com>
2023-09-02 16:12:06 +0200

    **Добавление фичей рандомного звука, цитаты hotfix 1. После npm install вылетают ошибки, которые фиксятся командой npm audit fix --force --Это меняет файлы package-lock.json и package-lock.json
    2. В src/core/modules добавлен random-sound.module.js
    -Модуль подключается к FreeSound API, и получает случайный звук из коллекции.
    -Для работы с ним необходим ключ получаемый после регистрации.
    -В данный момент добавлен мой ключ.
    
    3. Для этой функции нужно загрузить библиотеку axios командой npm install axios.
    --Это также изменит файлы package-lock.json и package-lock.json
    
    4. Изменён файл app.js добавлен импорт и вывод фичи в консоль.
    
    5. Исправлена функция add в menu.js связывающая клик и методов trigger

--- src/app.js
+++ src/app.js
@@ -1,20 +1,23 @@
 import './styles.css'
 
-import { ContextMenu } from './menu';
-import { TestModule } from './modules/test.module';
+import { ContextMenu } from './menu'
+import { TestModule } from './modules/test.module'
+import { RandomSoundModule } from './modules/random-sound.module'
+import { RandomQuoteModule } from './modules/random-quote.module'
 
-const contextMenu = new ContextMenu('#menu');
+const contextMenu = new ContextMenu('#menu')
 
-const addTestModule = new TestModule('add_text_block', 'Добваить тестовый блок с текстом');
+const addTestModule = new TestModule(
+  'add_text_block',
+  'Добавить тестовый блок с текстом'
+)
+const randomSoundModule = new RandomSoundModule('sound', 'Случайный звук')
+const randomQuoteModule = new RandomQuoteModule('quote', 'Случайная цитата')
 
-contextMenu.add(addTestModule);
-
-addTestModule.trigger = () => {
-    const newParagraph = document.createElement('p');
-    newParagraph.textContent = 'тестовый текст';
-    document.body.append(newParagraph);
-}
+contextMenu.add(addTestModule)
+contextMenu.add(randomSoundModule)
+contextMenu.add(randomQuoteModule)
 
 document.body.addEventListener('contextmenu', (event) => {
-    contextMenu.showContextMenu(event);
-});
+  contextMenu.showContextMenu(event)
+})
--- src/menu.js
+++ src/menu.js
@@ -1,35 +1,39 @@
 import { Menu } from './core/menu'
 
 export class ContextMenu extends Menu {
-    constructor(selector) {
-        super(selector);
-        this.menuItems = [];
-        this.menu = null;
-        this.createMenu();
-    }
+  constructor(selector) {
+    super(selector)
+    this.menuItems = []
+    this.menu = null
+    this.createMenu()
+  }
 
-    createMenu() {
-        this.menu = document.querySelector('#menu');
-    }
+  createMenu() {
+    this.menu = document.querySelector('#menu')
+  }
 
-    open() {
-        this.menu.classList.add('open');
-    }
+  open() {
+    this.menu.classList.add('open')
+  }
 
-    close() {
-        this.menu.classList.remove('open');
-    }
+  close() {
+    this.menu.classList.remove('open')
+  }
 
-    add(module) {
-        this.menuItems.push(module);
-        const menuItem = module.toHTML();
-        this.menu.append(menuItem)
-    }
+  add(module) {
+    this.menuItems.push(module)
+    const menuItem = document.createElement('li')
+    menuItem.classList.add('menu-item')
+    menuItem.dataset.type = module.type
+    menuItem.textContent = module.text
+    menuItem.addEventListener('click', () => module.trigger())
+    this.menu.append(menuItem)
+  }
 
-    showContextMenu(event) {
-        event.preventDefault();
-        this.open();
-        this.menu.style.left = `${event.clientX}px`;
-        this.menu.style.top = `${event.clientY}px`
-    }
+  showContextMenu(event) {
+    event.preventDefault()
+    this.open()
+    this.menu.style.left = `${event.clientX}px`
+    this.menu.style.top = `${event.clientY}px`
+  }
 }
