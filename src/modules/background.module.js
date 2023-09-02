32d658c02ccfba73d8c7e46131b5eddf9b47f071
lxspector <lxspector@gmail.com>
2023-09-02 23:02:02 +0200

    hotfix for background.js

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,31 +1,20 @@
-import { Module } from '../core/module';
-import { random } from './../utils';
+import { Module } from '../core/module'
+import { random } from './../utils'
+
 export class BackgroundModule extends Module {
   constructor(type, text) {
-    super(type, text);
+    super(type, text)
   }
 
-  getRandomColor(){
+  getRandomColor() {
     let red = random(0, 255)
     let green = random(0, 255)
     let blue = random(0, 255)
-    return `rgba(${red}, ${green}, ${blue})` 
+    return `rgba(${red}, ${green}, ${blue})`
   }
 
-  toHTML(){
+  trigger() {
     const body = document.querySelector('body')
-    const menuItem = document.createElement('li');
-    menuItem.className = 'menu-item';
-    menuItem.dataset.type = this.type;
-    menuItem.textContent = this.text;
-    menuItem.addEventListener('click', () => this.trigger(
-      body.style.background = this.getRandomColor()
-    ));
-    return menuItem;
-  }
-
-  changeBackgroundColor(){
-    const body = document.querySelector('body')
-    body.style.background = 'yellow'
+    body.style.background = this.getRandomColor()
   }
 }
